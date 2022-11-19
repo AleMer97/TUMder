6f996a98ff4bf963b8f453c41d7a62849a408248
Alex Mercier <alex.mercier@tum.de>
2022-11-19 12:43:40 +0100

    Changed profile

--- create-react-app/src/pages/Profile.js
+++ create-react-app/src/pages/Profile.js
@@ -5,16 +5,69 @@ import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import ProTip from '../ProTip';
 import Avatar from '@mui/material/Avatar';
+import { Button, Grid, Switch } from '@mui/material';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import TextField from '@mui/material/TextField';
 
 export default function Profile() {
 
- 
+  const [name, setName] = React.useState('Robert');
+
+  const handleChangeName = (event) => {
+    setName(event.target.value);
+  };
+
+  const [description, setDescription] = React.useState('Single and ready to mingle');
+
+  const handleChangeDescription = (event) => {
+    setDescription(event.target.name);
+  };
+
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
+    <Grid 
+      container 
+      direction="column" 
+      sx={{ my: 4 }}
+      justifyContent="flex-start"
+      alignItems="center"
+      spacing={3}
+    >
+      <Grid item>
         <Avatar sx={{ width: 56, height: 56 }}>OP</Avatar>
-        
-      </Box>
-    </Container>
+        <Button >Edit</Button>
+      </Grid>
+      
+      <Grid item>
+          <TextField
+            id="outlined-multiline-flexible"
+            label="Name"
+            maxRows={1}
+            value={name}
+            onChange={handleChangeName}
+          />
+      </Grid>
+      <Grid item>
+          <TextField
+            id="outlined-multiline-flexible"
+            label="Description"
+            multiline
+            maxRows={5}
+            minRows={3}
+            value={description}
+            onChange={handleChangeDescription}
+          />
+      </Grid>
+      <Grid item>
+        <FormGroup>
+          <FormControlLabel
+            value="start"
+            control={<Switch color="primary" />}
+            label="Mensa"
+            labelPlacement="start"
+          />        
+        </FormGroup>
+      </Grid>
+    </Grid>
   );
 }
