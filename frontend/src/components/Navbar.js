9489d4d18c3dac828112e248b4321e243f1afda1
Fabian Höltke <fabian.h001@gmail.com>
2022-11-20 00:40:00 +0100

    refactor repo structure

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import Paper from "@mui/material/Paper";
+import PersonSearchIcon from "@mui/icons-material/PersonSearch";
+import ForumIcon from "@mui/icons-material/Forum";
+import Avatar from "@mui/material/Avatar";
+
+function Navbar({ selected, user }) {
+  const [value, setValue] = React.useState(0);
+
+  React.useEffect(
+    (selected) => {
+      selected === "profile"
+        ? setValue(0)
+        : selected === "chats"
+        ? setValue(2)
+        : setValue(1);
+    },
+    [selected]
+  );
+
+  return (
+    <Paper
+      sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+      elevation={10}
+    >
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction
+          label="Profile"
+          icon={
+            <Avatar
+              alt="Profile Pic"
+              src={user.image}
+              sx={{ width: 24, height: 24 }}
+            />
+          }
+          component={Link}
+          to="/profile"
+        />
+        <BottomNavigationAction
+          label="TUMder"
+          icon={<PersonSearchIcon />}
+          component={Link}
+          to="/match"
+        />
+        <BottomNavigationAction
+          label="Chats"
+          icon={<ForumIcon />}
+          component={Link}
+          to="/chats"
+        />
+      </BottomNavigation>
+    </Paper>
+  );
+}
+
+export default Navbar;
---
+++ frontend/src/pages/NoPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function NoPage() {
+  return (
+    <div>
+      <h1>Error 404: This is the nopage page</h1>
+      <h2>Do you feel clever now?</h2>
+      <h3>Do you feel clever now?</h3>
+      <h4>Do you feel clever now?</h4>
+      <h5>Do you feel clever now?</h5>
+      <h6>Do you feel clever now?</h6>
+    </div>
+  );
+}
