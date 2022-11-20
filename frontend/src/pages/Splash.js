b8b184d64b51af1d7ada794d0ec7aba5bcb8a713
Fabian Höltke <fabian.h001@gmail.com>
2022-11-20 09:47:54 +0100

    better splash screen

--- frontend/src/pages/Splash.js
+++ frontend/src/pages/Splash.js
@@ -1,40 +1,48 @@
 import React from "react";
-import Box from '@mui/material/Box'
+import Box from "@mui/material/Box";
 import { Button } from "@mui/material";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Typography from "@mui/material/Typography";
 
 export default function Splash() {
   return (
     <div>
       <img
-        src={'/static/FunnyPeople.png'}
+        src={"/static/FunnyPeople.png"}
         alt={"Cool picture"}
         loading="lazy"
         width="100%"
-        style={{borderRadius:'25px'}}
+        style={{ borderRadius: "25px" }}
       />
-      <Box 
-        sx={{backgroundColor: '#3665A1', borderRadius: 15}}
-        style={{border:2, color:'white', textAlign:'center'}}
+      <Box
+        sx={{ backgroundColor: "#3665A1", borderRadius: 15 }}
+        style={{ border: 2, color: "white", textAlign: "center" }}
         m={4}
         p={1}
       >
-      Find your
-        <ul>
-          <li>StudyBuddy</li>
-          <li>MensaFriend</li>
-          <li>Tandem</li>
-        </ul>
+        Find your
+        <List>
+          <ListItem>
+            <Typography sx={{ ml: "25%" }} variant="h6">
+              StudyBuddy
+            </Typography>
+          </ListItem>
+          <ListItem>
+            <Typography sx={{ ml: "25%" }} variant="h6">
+              MensaFriend
+            </Typography>
+          </ListItem>
+          <ListItem>
+            <Typography sx={{ ml: "25%" }} variant="h6">
+              Tandem
+            </Typography>
+          </ListItem>
+        </List>
         ...or meet new students!
       </Box>
-      <Box 
-        justifyContent="center" 
-        alignItems="center" 
-        m={1}
-        display="flex"
-      >
-        <Button variant="outlined"
-          href="/match"
-        >
+      <Box justifyContent="center" alignItems="center" m={1} display="flex">
+        <Button variant="outlined" href="/match">
           Let's get started!
         </Button>
       </Box>
